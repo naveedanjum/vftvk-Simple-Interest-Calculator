fc2631f1557fb8d6cc726b4e4423bca217c2b7f8
naveedanjum <anjum.farrukh@gmail.com>
2021-07-30 20:15:21 +0500

    Versio 1.0

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var totalAmount = parseFloat(principal) + interest;
+
+    message = "If you deposit <b>"+principal +"</b>,<br/>At an interest rate of <b>"+rate+"%.</b>,<br/>"
+    message += "You will receive an amount of <b>"+totalAmount+"</b>,<br/>In the year "+year
+    var elResult = document.getElementById("result");
+    elResult.innerHTML = message;
+
+}
+
+function validateAmount(){
+    var principal = document.getElementById("principal").value;
+    if ( parseFloat(principal) <= 0 ) {
+        alert("You can not enter negative number");
+        document.getElementById("principal").value = null;
+        principal.focus();
+    }
     
 }
         
