f8da49fed4c205e3378940fab9f8e0a04f02d39b
naveedanjum <anjum.farrukh@gmail.com>
2021-07-31 21:20:06 +0500

    Fixed

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function compute()
     
         message = "If you deposit <mark>"+principal +"</mark>,<br/>At an interest rate of <mark>"+rate+"%.</mark>,<br/>";
         message += "You will receive an amount of Intrest: <mark>"+interest+"</mark>,<br/>In the year "+year +"<br/><br/>";
+        message += "<h4>Additional Feature</h4>";
         message += "Total Amount <b>(Principle + Intrest)</b> = <mark>"+totalAmount+"</mark>";
         var elResult = document.getElementById("result");
         elResult.innerHTML = message;
